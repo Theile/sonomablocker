e266a7575f5e71158e67cfdfa293bd54f57347d4
Theile <soren@theilgaard.dk>
2023-09-20 07:19:08 +0200

    Sonomablocker

---
+++ CONTRIBUTING.md
@@ -0,0 +1,9 @@
+# Contributing to venturablocker
+
+## Pull Requests (PRs)
+
+If you know of improvements to this app, please post a PR.
+
+## Issues
+
+If you encounter an issue please document it and write an issue.
---
+++ sonomablocker-remove.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+current_user_uid=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/UID :/ && ! /loginwindow/ { print $3 }' )
+
+launchd_item_path="/Library/LaunchAgents/dk.envo-it.venturablocker.plist"
+launchctl bootout gui/${current_user_uid} "${launchd_item_path}"
+
+rm -f "$launchd_item_path"
+rm -f /usr/local/bin/sonomablocker
+
+pkgutil --forget dk.envo-it.sonomablocker
