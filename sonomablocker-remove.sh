05fea882fcdb266baf416e53d5186bc223a79985
Theile <soren@theilgaard.dk>
2023-09-21 15:39:38 +0200

    Update sonomablocker-remove.sh

--- sonomablocker-remove.sh
+++ sonomablocker-remove.sh
@@ -2,7 +2,7 @@
 
 current_user_uid=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/UID :/ && ! /loginwindow/ { print $3 }' )
 
-launchd_item_path="/Library/LaunchAgents/dk.envo-it.venturablocker.plist"
+launchd_item_path="/Library/LaunchAgents/dk.envo-it.sonomablocker.plist"
 launchctl bootout gui/${current_user_uid} "${launchd_item_path}"
 
 rm -f "$launchd_item_path"
